f4af70b227aaa09d544760f89dc82d4438c6e021
jongs29 <silas.de.jong@hva.nl>
2022-04-29 18:04:05 +0200

    Update python config

--- users/silas/nixpkgs/modules/python/default.nix
+++ users/silas/nixpkgs/modules/python/default.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }:
 
 let
+    mach-nix = import (builtins.fetchGit {
+        url = "https://github.com/DavHau/mach-nix";
+        ref = "refs/tags/3.4.0";
+    }) {};
     my-python-packages = python-packages: with python-packages; [
         requests
         pulsectl
@@ -8,10 +12,20 @@ let
         ipykernel
         # other python packages you want
     ];
-    python-with-my-packages = pkgs.python3.withPackages my-python-packages;in
+    python-with-my-packages = pkgs.python3.withPackages my-python-packages;
+in
 {
     home.packages = [
-        python-with-my-packages
+        # python-with-my-packages
+        (mach-nix.mkPython {
+            requirements = ''
+                requests
+                pulsectl
+                papis-python-rofi
+                ipykernel
+                pyqt5
+        '';
+        })
         pkgs.nodePackages.pyright
     ];
 }

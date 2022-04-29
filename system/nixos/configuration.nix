9b612dc51df2a0e874767aeff17dacf56568cad1
jongs29 <silas.de.jong@hva.nl>
2022-04-29 18:02:41 +0200

    Disabled mosc because it messed with plover input

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -153,11 +153,11 @@
 
   # Select internationalisation properties.
   i18n = {
-      defaultLocale = "en_US.UTF-8";
-      inputMethod = {
-          enabled = "fcitx";
-          fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
-      };
+      # defaultLocale = "en_US.UTF-8";
+      # inputMethod = {
+      #     enabled = "fcitx";
+      #     fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
+      # };
   };
 
   # console = {

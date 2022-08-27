393f6a57c4589f822aac59b88f03abfb9c0b66a4
jongs29 <silas.de.jong@hva.nl>
2022-08-27 19:30:57 +0200

    Removed unnessary files

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
                         }
                         ({ pkgs, ... }: {
                             nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
-                            # environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
                         })
                     ];
                 };
